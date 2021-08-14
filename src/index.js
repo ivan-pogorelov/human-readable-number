36348f69d69a10d94705f3d52117ee02f6a4c95e
ivan-pogorelov <ivanpogorelov97a@gmail.com>
2021-08-14 19:42:25 +0300

    refactor: change algorithm

--- src/index.js
+++ src/index.js
@@ -1,66 +1,43 @@
 module.exports = function toReadable (number) {
 
-var string = number.toString(),
-    units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words;
-
-var and = 'and';
-
-if (parseInt(string) === 0) {
-    return 'zero';
-}
-
-
-units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-
-tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
-scales = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion'];
-
-start = string.length;
-chunks = [];
-while (start > 0) {
-    end = start;
-    chunks.push(string.slice((start = Math.max(0, start - 3)), end));
-}
-
-chunksLen = chunks.length;
-if (chunksLen > scales.length) {
-    return '';
-}
-
-words = [];
-for (i = 0; i < chunksLen; i++) {
-
-    chunk = parseInt(chunks[i]);
-
-    if (chunk) {
-
-        ints = chunks[i].split('').reverse().map(parseFloat);
-
-        if (ints[1] === 1) {
-            ints[0] += 10;
-        }
-
-        if ((word = scales[i])) {
-            words.push(word);
-        }
-
-        if ((word = units[ints[0]])) {
-            words.push(word);
-        }
-
-        if ((word = tens[ints[1]])) {
-            words.push(word);
-        }
-
-        if ((word = units[ints[2]])) {
-            words.push(word + ' hundred');
-        }
-
+    let str = number.toString();
+    const result = [];
+    const dict = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+    
+    if (str === '0') {
+        return dict[0];
     }
 
-}
+    if (str.length === 3) {
+        result.push(dict[str[0]]);
+        result.push('hundred');
+        str = str.substring(1);
+    }
 
-return words.reverse().join(' ');
+    if (str.length === 2) {
+        if (dict[str]) {
+            result.push(dict[str]);
+            return result.join(' ');
+        }
+
+        const dictForTens = [
+            '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+        ];
+
+        if (str[0] !== '0') {
+            result.push(dictForTens[str[0]]);
+        }
+
+        str = str.substring(1);
+    }
+
+    if (str !== '0') {
+        result.push(dict[str]);
+    }
+    
+    return result.join(' ');
 
 }
